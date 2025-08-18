89700293ce3c6e50d64d35394d3a993afbe3e608
Aariyana <48056044+Aariyana@users.noreply.github.com>
2025-08-18 13:52:47 +0530

    Update db.py

--- src/db.py
+++ src/db.py
@@ -3,3 +3,8 @@ from pymongo import MongoClient
 
 client = MongoClient(os.getenv("MONGODB_URI"))
 db = client[os.getenv("MONGODB_DB", "edubot")]
+#Define collections
+users_col = db["users"]
+referrals_col = db["referrals"]
+premium_col = db["premium"]
+quiz_col = db["quiz"]
