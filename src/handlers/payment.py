8dfb33833d591b6e8207bb157ebd611f60f469f8
Aariyana <48056044+Aariyana@users.noreply.github.com>
2025-08-20 02:24:42 +0530

    Update payment.py

--- src/handlers/payment.py
+++ src/handlers/payment.py
@@ -1,9 +1,12 @@
 from aiogram import Router, types
+from aiogram.filters import Command
 
 router = Router()
 
-@router.message(commands=["pay"])
+@router.message(Command("pay"))
 async def cmd_pay(message: types.Message):
     await message.answer(
-        "💰 Payment Method:\n\nSend UPI to: 7002561086@okbizaxis\n\nAfter payment, send screenshot to @abhijitedu"
+        "💳 Payment Method:\n\n"
+        "Send UPI to: 7002561086@okbizaxis\n\n"
+        "After payment, send screenshot to @abhijitedu"
     )
